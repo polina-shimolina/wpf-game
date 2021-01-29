a72620c7d75bf26085a337bfe44e239cf5680d06
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-01-29 16:32:30 +0300

    2

--- App/App/MainWindow.xaml.cs
+++ App/App/MainWindow.xaml.cs
@@ -24,30 +24,56 @@ namespace App
         {
             InitializeComponent();
         }
+
+
+        private void L_KeyDown(object sender, KeyEventArgs e)
+        {
+            double left = l.Margin.Left;
+            double right = l.Margin.Right;
+            double top = l.Margin.Top;
+            double bottom = l.Margin.Bottom;
+            switch (e.Key)
+            {
+                case Key.A:
+                    left -= 2;
+                    break;
+                case Key.D:
+                    left += 2;
+                    break;
+                //прыжок
+                default:
+                    break;
+            }
+            l.Margin = new Thickness(left, top, right, bottom);
+        }
     }
 
-    private void L_KeyUp(object sender, KeyEventArgs e)
-    {
-        if (Keyboard.IsKeyUp(Key.A))
-        {
-            l.Margintest.Margin = new Thickness(-2, 0, 0, 0);
-        }
-        if (Keyboard.IsKeyUp(Key.D))
-        {
-            l.Margintest.Margin = new Thickness(+2, 0, 0, 0);
-        }
-        if (Keyboard.IsKeyUp(Key.Space))
-        {
-            //прыжок
-        }
-        if (Keyboard.IsKeyUp(Key.A) && Keyboard.IsKeyUp(Key.W))
-        {
-            //прыжок
-        }
-        if (Keyboard.IsKeyUp(Key.D) && Keyboard.IsKeyUp(Key.W))
-        {
-            //прыжок
-        }
+        //private void L_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (Keyboard.IsKeyDown(Key.A))
+        //    {
+        //        l.Margin = new Thickness(-2, 0, 0, 0);
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.D))
+        //    {
+        //        l.Margin = new Thickness(+2, 0, 0, 0);
+        //    }
+        //    if (Keyboard.IsKeyUp(Key.Space))
+        //    {
+        //        //прыжок
+        //    }
+        //    if (Keyboard.IsKeyUp(Key.A) && Keyboard.IsKeyUp(Key.W))
+        //    {
+        //        //прыжок
+        //    }
+        //    if (Keyboard.IsKeyUp(Key.D) && Keyboard.IsKeyUp(Key.W))
+        //    {
+        //        //прыжок
+        //    }
 
-    }
+        //}
+
 }
+
+   
+
